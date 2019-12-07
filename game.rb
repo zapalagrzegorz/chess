ac878ff01655f6565048f1392db659126c8e3d5e
Grzegorz Zapala <zapala.grzegorz@gmail.com>
2019-12-07 14:21:06 +0100

    Fulfil UML spec

--- game.rb
+++ game.rb
@@ -7,6 +7,8 @@ require_relative "human_player.rb"
 
 # Wrapping class
 class Game
+  attr_reader :board, :display, :players
+
   def initialize
     @board = Board.new
     @display = Display.new(@board)
@@ -44,28 +46,8 @@ class Game
   def swap_turn!
     @current_player = @current_player == @players[:player1] ? @players[:player2] : @players[:player1]
   end
+end
 
-  def move_player
-    user_input = nil
-    until user_input
-      puts "#{current_player}, please give new start and end position like 0,0 2,0"
-      user_input = STDIN.gets.chomp
-      user_input = parse_input(user_input)
-      puts
-    end
-
-    @board.move_piece[user_input[start_pos], user_input[end_pos]]
-  end
-
-  def parse_input(user_input)
-    return nil unless user_input[2] =~ /^[0-7],[0-7]\s[0-7],[0-7]/
-
-    start_pos = user_input[0..2].split(",").map { |pos| Integer(pos) }
-    end_pos = user_input[4..6].split(",").map { |pos| Integer(pos) }
-
-    return {
-             "start_pos" => start_pos,
-             "end_pos" => end_pos,
-           }
-  end
+if $PROGRAM_NAME == __FILE__
+  Game.new.play
 end
